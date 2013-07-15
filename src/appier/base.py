1dd1d46df0eaac4117c0ecd51c135d7a710bdbc4
joamag <joamag@gmail.com>
2013-07-15 19:10:32 +0100

    small code change

--- src/appier/base.py
+++ src/appier/base.py
@@ -34,10 +34,32 @@ LOGGING_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
 the bot, these operations are going to be handled by
 multiple stream handlers """
 
+class MemoryHandler(logging.Handler):
+
+    MAX_LENGTH = 1000
+
+    def __init__(self, level = logging.NOTSET):
+        logging.Handler.__init__(self, level = level)
+        self.messages = []
+
+        formatter = logging.Formatter(LOGGING_FORMAT)
+        self.setFormatter(formatter)
+
+    def emit(self, record):
+        message = self.format(record)
+        self.messages.insert(0, message)
+        messages_l = len(self.messages)
+        if messages_l > MemoryHandler.MAX_LENGTH:
+            self.messages.pop()
+
+    def get_latest(self, count = 100):
+        return self.messages[:count]
+
 class App(object):
 
     def __init__(self, name = None):
         self.name = name or self.__class__.__name__
+        self.handler = MemoryHandler()
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(logging.DEBUG)
         self.logger.addHandler(self.handler)
