9aaf6f3ee0678187904345bb0e977a5906b46936
joamag <joamag@gmail.com>
2014-01-02 15:31:20 +0000

    new field based validation

--- src/appier/validation.py
+++ src/appier/validation.py
@@ -215,6 +215,30 @@ def is_regex(name, regex):
         )
     return validation
 
+def field_gt(name, field):
+    def validation(object, ctx):
+        name_v = object.get(name, None)
+        field_v = object.get(field, None)
+        if name_v == None: return True
+        if field_v == None: return True
+        if name_v > field_v: return True
+        raise exceptions.ValidationInternalError(
+            name, "must be greater than %s" % field
+        )
+    return validation
+
+def field_lt(name, field):
+    def validation(object, ctx):
+        name_v = object.get(name, None)
+        field_v = object.get(field, None)
+        if name_v == None: return True
+        if field_v == None: return True
+        if name_v < field_v: return True
+        raise exceptions.ValidationInternalError(
+            name, "must be less than %s" % field
+        )
+    return validation
+
 def string_gt(name, size):
     def validation(object, ctx):
         value = object.get(name, None)
