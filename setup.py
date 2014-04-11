55656e15c8933c19eb5f027e0c05cae216a335b3
joamag <joamag@gmail.com>
2014-04-11 12:57:38 +0100

    new version value

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier",
-    version = "0.4.1",
+    version = "0.4.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
--- src/appier/base/common.py
+++ src/appier/base/common.py
@@ -78,7 +78,7 @@ NAME = "appier"
 """ The name to be used to describe the framework while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "0.4.1"
+VERSION = "0.4.2"
 """ The version of the framework that is currently installed
 this value may be used for debugging/diagnostic purposes """
 
