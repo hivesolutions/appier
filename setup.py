4df3b509fe842627c52e62314a1b91e90bc40a5b
joamag <joamag@gmail.com>
2014-04-22 17:28:06 +0100

    new python version in setup file

--- setup.py
+++ setup.py
@@ -71,6 +71,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7"
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
     ]
 )
