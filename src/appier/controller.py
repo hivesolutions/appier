481d13adb1be140123c452a6547d1e4adcbf9cbc
joamag <joamag@gmail.com>
2014-04-22 17:29:20 +0100

    new python 3 compatibility

--- src/appier/controller.py
+++ src/appier/controller.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import observer
+from appier import observer
 
 class Controller(observer.Observable):
     """
