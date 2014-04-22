35418350c49869391f4ea5b69d0963c5b6df1a0d
joamag <joamag@gmail.com>
2014-04-22 17:01:06 +0100

    new import model

--- examples/complex/controllers/__init__.py
+++ examples/complex/controllers/__init__.py
@@ -34,6 +34,6 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import base
+from . import base
 
-from base import *
+from .base import *
