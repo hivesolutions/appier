dff44631ea16ee47c71395cd877161076928872f
joamag <joamag@gmail.com>
2014-04-23 10:52:51 +0100

    new python 3 compliant version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier",
-    version = "0.4.12",
+    version = "0.5.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
