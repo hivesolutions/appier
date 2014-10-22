5284b2fa70c25ff4cc1b11328103f2cce8f9edca
joamag <joamag@gmail.com>
2014-10-22 17:30:07 +0100

    new license, changed to apache

--- src/appier/test/__init__.py
+++ src/appier/test/__init__.py
@@ -7,17 +7,17 @@
 # This file is part of Hive Appier Framework.
 #
 # Hive Appier Framework is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
 #
 # Hive Appier Framework is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# Apache License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Hive Appier Framework. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
 
 __version__ = "1.0.0"
 """ The version of the module """
@@ -31,5 +31,5 @@ __date__ = "$LastChangedDate$"
 __copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
 """ The copyright for the module """
 
-__license__ = "GNU General Public License (GPL), Version 3"
+__license__ = "Apache License, Version 2.0"
 """ The license for the module """
