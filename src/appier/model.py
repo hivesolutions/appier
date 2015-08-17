462b26a92f11a3a74de190db43b0ee9e38c9cbd7
João Magalhães <joamag@hive.pt>
2015-08-17 22:13:50 +0200

    new values

--- src/appier/model.py
+++ src/appier/model.py
@@ -215,7 +215,7 @@ class Model(legacy.with_meta(meta.Ordered, observer.Observable)):
     """
 
     def __new__(cls, *args, **kwargs):
-        instance = super(Model, cls).__new__(*args, **kwargs)
+        instance = super(Model, cls).__new__(cls, *args, **kwargs)
         instance.__dict__["model"] = {}
         return instance
 
