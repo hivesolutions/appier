2eee1fd7e1123b218f730ac42b05bd074cb47e2e
joamag <joamag@gmail.com>
2015-09-11 23:15:57 +0100

    new model information

--- src/appier/model.py
+++ src/appier/model.py
@@ -428,13 +428,14 @@ class Model(legacy.with_meta(meta.Ordered, observer.Observable)):
 
     @classmethod
     def get(cls, *args, **kwargs):
-        fields, eager, map, rules, meta, build, skip, limit, sort, raise_e = cls._get_attrs(kwargs, (
+        fields, eager, map, rules, meta, build, fill, skip, limit, sort, raise_e = cls._get_attrs(kwargs, (
             ("fields", None),
             ("eager", None),
             ("map", False),
             ("rules", True),
             ("meta", False),
             ("build", True),
+            ("fill", True),
             ("skip", 0),
             ("limit", 0),
             ("sort", None),
@@ -466,8 +467,8 @@ class Model(legacy.with_meta(meta.Ordered, observer.Observable)):
             )
         if not model and not raise_e: return model
         cls.types(model)
-        cls.fill(model)
-        build and cls.build(model, map = map, rules = rules, meta = meta)
+        if fill: cls.fill(model)
+        if build: cls.build(model, map = map, rules = rules, meta = meta)
         if eager: model = cls._eager(model, eager)
         return model if map else cls.old(model = model, safe = False)
 
@@ -506,7 +507,7 @@ class Model(legacy.with_meta(meta.Ordered, observer.Observable)):
         )
         models = [cls.types(model) for model in models]
         if fill: models = [cls.fill(model) for model in models]
-        build and [cls.build(model, map = map, rules = rules, meta = meta) for model in models]
+        if build: [cls.build(model, map = map, rules = rules, meta = meta) for model in models]
         if eager: models = cls._eager(models, eager)
         models = models if map else [cls.old(model = model, safe = False) for model in models]
         return models
