6c6579b934db9f8250ea4677e388ac0f7328b5c5
joamag <joamag@gmail.com>
2015-10-11 21:20:42 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier",
-    version = "0.9.82",
+    version = "0.9.83",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
