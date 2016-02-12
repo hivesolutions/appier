a1ad16826f3e99f5efdb86d9169c34db0875b4b8
joamag <joamag@hive.pt>
2016-02-12 17:59:32 +0000

    new version

--- src/appier/base.py
+++ src/appier/base.py
@@ -90,7 +90,7 @@ NAME = "appier"
 """ The name to be used to describe the framework while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "1.2.4"
+VERSION = "1.2.5"
 """ The version of the framework that is currently installed
 this value may be used for debugging/diagnostic purposes """
 
