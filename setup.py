fc9d2a5f68ba0cbada09cb93ded477089e90fb00
joamag <joamag@hive.pt>
2016-03-22 17:25:23 +0000

    small bug fix

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier",
-    version = "1.3.5",
+    version = "1.3.6",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
