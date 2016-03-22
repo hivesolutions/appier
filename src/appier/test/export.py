13b8fb2a5ab2372ed2c15ed456311a9448a07bdf
joamag <joamag@hive.pt>
2016-03-22 16:25:41 +0000

    initial support for the import test

---
+++ src/appier/test/export.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import json
+import unittest
+
+import appier
+
+class ExportTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = appier.App()
+
+    def tearDown(self):
+        adapter = appier.get_adapter()
+        adapter.drop_db()
+
+    def test_import(self):
+        structure = {
+            "person:id" : dict(_id = "person:id", seq = 11),
+            "account:id" : dict(_id = "account:id", seq = 33)
+        }
+        data = json.dumps(structure)
+
+        adapter = appier.get_adapter()
+        manager = appier.ExportManager(
+            adapter,
+            multiple = self.app.resolve()
+        )
+
+        collection = adapter.collection("counter")
+        manager._import_single(collection, data, key = "_id")
+
+        values = collection.find()
+        values = [value for value in values]
+
+        self.assertEqual(type(values), list)
+        self.assertEqual(len(values), 2)
+
+        value = collection.find_one(filter = dict(_id = "person:id"))
+
+        self.assertEqual(type(value), dict)
+        self.assertEqual(value["seq"], 11)
