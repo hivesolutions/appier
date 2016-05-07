818f0a9e901961d615b07881c6e9fa6597352baa
joamag <joamag@gmail.com>
2016-05-07 19:44:23 +0100

    new location support

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "appier",
-    version = "1.4.5",
+    version = "1.4.6",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
