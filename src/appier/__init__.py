6ab89a1e78ec98dda8159f1eaf2b77a40bd5b883
joamag <joamag@hive.pt>
2016-12-30 15:02:23 +0000

    initial queue support

--- src/appier/__init__.py
+++ src/appier/__init__.py
@@ -57,6 +57,7 @@ from . import model
 from . import mongo
 from . import observer
 from . import part
+from . import queuing
 from . import redisdb
 from . import request
 from . import scheduler
@@ -99,6 +100,7 @@ from .model import Model, LocalModel, Field, link, operation, field
 from .mongo import Mongo, get_connection, reset_connection, get_db, drop_db, object_id, dumps
 from .observer import Observable
 from .part import Part
+from .queuing import Queue, MemoryQueue, MultiprocessQueue, AMQPQueue
 from .redisdb import Redis
 from .request import CODE_STRINGS, Request, MockRequest
 from .scheduler import Scheduler
---
+++ src/appier/queuing.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (c) 2008-2017 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2017 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+from . import exceptions
+
+class Queue(object):
+
+    def push(self, value, priority = None):
+        raise exceptions.NotImplementedError()
+
+    def pop(self, block = True):
+        raise exceptions.NotImplementedError()
+
+    def register(self, callback):
+        raise exceptions.NotImplementedError()
+
+class MemoryQueue(Queue):
+    pass
+
+class MultiprocessQueue(Queue):
+
+    def __init__(self):
+        try: import queue
+        except ImportError: import Queue as queue
+        Queue.__init__(self)
+        self._queue = queue.PriorityQueue()
+
+    def push(self, value, priority = None):
+        self._queue.put((priority, value))
+
+    def pop(self, block = True):
+        _priority, value = self._queue.get(block)
+        return value
+
+class AMQPQueue(Queue):
+    pass
