23dc82a6b48390d9f0c5a059d141447d5f8f54b4
joamag <joamag@hive.pt>
2016-12-30 16:07:25 +0000

    new job identifier support

--- src/appier/queuing.py
+++ src/appier/queuing.py
@@ -37,6 +37,8 @@ __copyright__ = "Copyright (c) 2008-2017 Hive Solutions Lda."
 __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
+import uuid
+
 from . import exceptions
 
 class Queue(object):
@@ -44,7 +46,7 @@ class Queue(object):
     def push(self, value, priority = None):
         raise exceptions.NotImplementedError()
 
-    def pop(self, block = True):
+    def pop(self, block = True, full = False):
         raise exceptions.NotImplementedError()
 
     def register(self, callback):
@@ -61,12 +63,15 @@ class MultiprocessQueue(Queue):
         Queue.__init__(self)
         self._queue = queue.PriorityQueue()
 
-    def push(self, value, priority = None):
-        self._queue.put((priority, value))
+    def push(self, value, priority = None, identify = False):
+        if identify: identifier = str(uuid.uuid4())
+        else: identifier = None
+        self._queue.put((priority, identifier, value))
+        return identifier
 
-    def pop(self, block = True):
-        _priority, value = self._queue.get(block)
-        return value
+    def pop(self, block = True, full = False):
+        priority, identifier, value = self._queue.get(block)
+        return (priority, identifier, value) if full else value
 
 class AMQPQueue(Queue):
     pass
