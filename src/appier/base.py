ff5218308782ca343ac7daff862befed4c96f235
joamag <joamag@gmail.com>
2017-03-02 10:50:43 +0000

    new reloading of logging for fork

--- src/appier/base.py
+++ src/appier/base.py
@@ -606,6 +606,7 @@ class App(
     def fork(self):
         if self.manager: self.manager.start()
         if self.adapter: self.adapter.reset()
+        self._load_logging(self.level)
 
     def loop(self, callable = lambda: time.sleep(60)):
         # prints a small information message about the event loop that is
