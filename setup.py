110085b29e51368e4071f5d1fa6b895470d182f0
joamag <joamag@hive.pt>
2017-12-12 19:34:29 +0000

    new version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def read_file(path):
 
 setuptools.setup(
     name = "appier",
-    version = "1.13.36",
+    version = "1.13.37",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
--- src/appier/base.py
+++ src/appier/base.py
@@ -97,7 +97,7 @@ NAME = "appier"
 """ The name to be used to describe the framework while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "1.13.36"
+VERSION = "1.13.37"
 """ The version of the framework that is currently installed
 this value may be used for debugging/diagnostic purposes """
 
