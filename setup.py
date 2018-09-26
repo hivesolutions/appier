f613259e9faaa7f5187319f853e8d016033daea3
João Magalhães <joamag@hive.pt>
2018-09-26 17:50:50 +0100

    new setup file version

--- setup.py
+++ setup.py
@@ -86,7 +86,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
     long_description = read_file("README.rst")
 )
