d15b493205399f63983c714798dc2a683f3ea841
joamag <joamag@hive.pt>
2018-12-07 10:13:01 +0000

    new version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def read_file(path):
 
 setuptools.setup(
     name = "appier",
-    version = "1.17.30",
+    version = "1.17.31",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
