5972b087f82a86c86b3ab9b323080bfe597b169e
joamag <joamag@gmail.com>
2019-01-02 14:04:29 +0000

    New version 1.17.42

--- src/appier/base.py
+++ src/appier/base.py
@@ -98,7 +98,7 @@ NAME = "appier"
 """ The name to be used to describe the framework while working
 on its own environment, this is just a descriptive value """
 
-VERSION = "1.17.41"
+VERSION = "1.17.42"
 """ The version of the framework that is currently installed
 this value may be used for debugging/diagnostic purposes """
 
