422d53faf27cb9542fae3c4ae14f9c1092b30a5e
joamag <joamag@hive.pt>
2019-07-24 19:26:45 +0100

    New touch

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 # Hive Appier Framework is free software: you can redistribute it and/or modify
 # it under the terms of the Apache License as published by the Apache
 # Foundation, either version 2.0 of the License, or (at your option) any
-# later version.
+# later version. 
 #
 # Hive Appier Framework is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
