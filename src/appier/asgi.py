e84425c66b95853826cb2e6bef88556b70b2667d
joamag <joamag@hive.pt>
2019-11-12 13:15:44 +0000

    New send operation support

--- src/appier/asgi.py
+++ src/appier/asgi.py
@@ -83,6 +83,10 @@ class ASGIApp(object):
         )
         server.run()
 
+    async def send(self, data, content_type = None):
+        if content_type: self.request_ctx.set_content_type(content_type)
+        return await self.request_ctx.send(data)
+
     async def app_asgi(self, *args, **kwargs):
         return await self.application_asgi(*args, **kwargs)
 
