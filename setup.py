5f5f41b1d43084f815c8c3b8511f556f2a34c78d
João Magalhães <joamag@gmail.com>
2020-10-20 13:09:45 +0100

    Version 1.20.5

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def read_file(path):
 
 setuptools.setup(
     name = "appier",
-    version = "1.20.4",
+    version = "1.20.5",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Appier Framework",
