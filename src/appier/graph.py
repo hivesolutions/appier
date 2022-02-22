e3eba8999e3d4ad1c5617b1d61b8248b50f8a5b6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-22 12:00:01 +0000

    fix: type checking

--- src/appier/graph.py
+++ src/appier/graph.py
@@ -1,3 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (c) 2008-2022 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2021 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
 import math
 
 import appier
@@ -32,8 +71,8 @@ class Graph(object):
         for edge in edges:
             if len(edge) < 2: continue
             src, dst = edge[0], edge[1]
-            cost = edge[2] if len(edge) > 2 else 1
-            bidirectional = edge[3] if len(edge) > 3 else False
+            cost = edge[2] if len(edge) > 2 and isinstance(edge[2], int) else 1
+            bidirectional = edge[3] if len(edge) > 3 and isinstance(edge[3], bool) else False
             self.add_edge(src, dst, cost = cost, bidirectional = bidirectional)
 
     def add_edge(self, src, dst, cost = 1, bidirectional = False):
