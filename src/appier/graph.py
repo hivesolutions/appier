639686929ea098ec96cdfe7fed1cdebd27f03b3d
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-22 12:00:45 +0000

    fix: copyright date

--- src/appier/graph.py
+++ src/appier/graph.py
@@ -31,7 +31,7 @@ __revision__ = "$LastChangedRevision$"
 __date__ = "$LastChangedDate$"
 """ The last change date of the module """
 
-__copyright__ = "Copyright (c) 2008-2021 Hive Solutions Lda."
+__copyright__ = "Copyright (c) 2008-2022 Hive Solutions Lda."
 """ The copyright for the module """
 
 __license__ = "Apache License, Version 2.0"
