0e5452eceba583fe88ec5eb7f2ea9da0c861f3c8
João Magalhães <joamag@gmail.com>
2024-06-02 11:25:16 +0100

    version: 1.34.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+*
+
+## [1.34.2] - 2024-06-02
+
+### Fixed
+
 * Double start execution for ASGI applications
 
 ## [1.34.1] - 2024-06-02
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def read_file(path):
 
 setuptools.setup(
     name="appier",
-    version="1.34.1",
+    version="1.34.2",
     author="Hive Solutions Lda.",
     author_email="development@hive.pt",
     description="Appier Framework",
